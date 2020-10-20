6ebeb637d1a3de38209affdf5f4d848498d17737
Karan <karanoza94@gmail.com>
2020-10-20 15:02:34 +0530

    commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Assignment
+
+This is login and register page using angular.
+
+## You can start the app by following steps.:
+
+1. run npm install
+
+2. npm start
+
+3. Goto browser: localhost:8080
---
+++ spring login/src/app/models/user.ts
@@ -0,0 +1,10 @@
+﻿export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    mobile: string;
+    email: string;
+    token: string;
+}
